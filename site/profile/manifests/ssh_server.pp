7a8f0fbfa84838ffddfabbc2fd413f1a5339adde
ymac3 <102021459+ymac3@users.noreply.github.com>
2023-02-01 10:51:15 +1100

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+   package {'openssl-server':
+      ensure => present,
+   }
+   service { 'sshd':
+      ensure => 'running',
+      enable => 'true',
+   }
+   ssh_authorised_key { 'root@master.puppet.vm':
+      ensure => present,
+      user   => 'root',
+      type   => 'ssh-rsa'
+      key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCh+yZpe4ShxoAA4sJBHX6Is7y4xyiHpfPMKtPweBOXVWvdFmby3cLmlInzrR0ejE9A/NRVnhPRXOCHjuXRPcRGeWwn5DrtFr+kVmOkxsXzP09at58dQORIrHTkTXNX84DfmCml1J5cgF4FOgfntb4pBQliCs7AIHmrorDvSZumh01i+LN6B//OqEv1uOyL+gguZCVFfBjH0r0G1ogrA6iBrKSPHmngjiSqv7H7I9518bXIi5PQzaKRcCYVFIzASZq1hIZmi5lD6XOH9mtnWht2CBHzd8o3Nw2tY1IcISCM4FMhPrb/y3SrC0gh8S0LgJxSWilGo9rM1pc2TDGJx1dl'
+   }
+}
